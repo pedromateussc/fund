74d78db5ea8042f59b288ba8484f88908ace9920
pedromateussc <54292298+pedromateussc@users.noreply.github.com>
2019-10-10 10:46:53 -0300

    Create for2.py

---
+++ for2.py
@@ -0,0 +1,13 @@
+#Imprimir os 10 primeiros n naturais
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in numeros:
+    print(i)
+
+
+#Facilitando a msm idea com a fun range
+print("========")
+
+for i  in range(100):
+    print(i)
