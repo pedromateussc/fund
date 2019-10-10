d9119915ffdc8020fc8e8022fcdcf730b87b52f5
pedromateussc <54292298+pedromateussc@users.noreply.github.com>
2019-10-10 10:46:26 -0300

    Create for1.py

---
+++ for1.py
@@ -0,0 +1,9 @@
+#utilizando os valores de uma lista com um laço
+#print(slecione 1 das cer pelo codigo, antes do for
+
+#cervejaescolhida = input(), dps do for
+cervejas = ["heineken", " corona", " petra", " eisenbah", " ns2"]
+for cerveja in cervejas:
+    print(cerveja)
+
+
